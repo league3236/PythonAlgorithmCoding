218a78788386001357eaac206e75573d2bdf8b9d
KwakSeungHyeok <league3236@gmail.com>
2019-09-24 07:17:41 +0000

    Create stringtoint.cpp

---
+++ stringtoint.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    streamstring ss(s);
+    ss >> answer;
+    return answer;
+}
