249c97156de1f5c89bd249413e303c46e4080f20
league3236 <league3236@gmail.com>
2020-07-29 21:31:29 +0900

    quick정렬 중간저장/200729

---
+++ quicksort.py
@@ -0,0 +1,14 @@
+
+
+numbers = [40, 35, 27, 50, 75]
+
+# def quickSort(array):
+#     if len(array) < 2:
+#         return array
+#     else:
+#         return array
+# result = quickSort(numbers)
+# print(result)
+
+list = [number for number in numbers if number < 35]
+print(list)
