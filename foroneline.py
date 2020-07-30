5e0349e5eb14387f877f938bbce78e4aface71a7
league3236 <league3236@gmail.com>
2020-07-30 22:49:52 +0900

    foroneline

--- foroneline.py
+++ foroneline.py
@@ -6,3 +6,11 @@ print(list2)
 
 temp = list([2,3,4,5])
 print(temp)
+
+# 이중 for문 한줄로 작성
+
+list3 = [i*j for j in list1 for i in list1]
+print(list3)
+
+list4 = [i for i in list1 if i>3]
+print(list4)
