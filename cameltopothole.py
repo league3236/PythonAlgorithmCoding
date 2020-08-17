939b93f45e040bb3c4a81864a143a0d9d6c6e250
league3236 <league3236@gmail.com>
2020-08-18 00:00:03 +0900

    cameltopothole

--- cameltopothole.py
+++ cameltopothole.py
@@ -2,6 +2,14 @@
 
 def solution(s):
     answer = ''
+    for idx, value in enumerate(s):
+        print(value.isupper())
+        if value.isupper() or value.isdigit():
+            answer=answer+'_'
+            print('hi')
+        if value.isupper():
+            value = value.lower()
+        answer = answer+value
     return answer
 
 result = solution('codingDojang')
