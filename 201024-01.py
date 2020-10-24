511e178b685d51bb3be3a2ae4683767f791273b0
KwakSeungHyeok <league3236@gmail.com>
2020-10-24 23:05:28 +0900

    Update 201024-01.py

--- 201024-01.py
+++ 201024-01.py
@@ -12,9 +12,17 @@
 import heapq
 
 def solution(scoville, K):
+    answer = 0
     heap = []
-    for num in scovile:
+    for num in scoville:
+        heapq.heappush(heap, num)
 
+    while heap[0] < K:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
+        except IndexError:
+            return -1
+        answer += 1
     return answer
 
 print(solution([1,2,3,9,10,12], 7))
