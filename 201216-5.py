9b014e0f19795d8c8f3157841ee79c21a8c69d75
league3236 <league3236@gmail.com>
2020-12-17 22:14:16 +0900

    201216

--- 201216-5.py
+++ 201216-5.py
@@ -1,9 +1,17 @@
 # https://www.techiedelight.com/find-duplicate-element-limited-range-array/
 
 
+def findDuplicate(A):
+    visited = [False] * (len(A)-1)
+    # print(visited)
+    for i in A:
+        if visited[i-1]:
+            return i 
+        visited[i-1] = True
 
 if __name__ == '__main__':
     A = [1, 2, 3, 4, 4]
 
-    a = [False]*10
-    print(a)
+    print("Duplicate element is", findDuplicate(A))
+
+
