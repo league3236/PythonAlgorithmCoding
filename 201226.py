a5a1b26b467bfef648c42cf6139c45bd72451e0f
league3236 <league3236@gmail.com>
2020-12-26 14:03:28 +0900

    201226

--- 201226.py
+++ 201226.py
@@ -4,12 +4,14 @@ def binary_search(sorted_list, searchnum):
     start = 0
     end = len(sorted_list)-1
 
-    middle = start+(end-start)//2
-
-    print(middle)
-
-    # while(start <= end):
-    #     middle = (e)//2
+    while(start <= end):
+        middle = start+(end-start)//2
+        if searchnum == sorted_list[middle]:
+            return middle
+        elif searchnum > sorted_list[middle]:
+            start = middle+1
+        else:
+            end = middle-1
     return -1
 
 alist = [1,2,3,4,5,6,7,8]
