8f9e33644b2cf33edcaff81c3ed92e7a8676befc
league3236 <league3236@gmail.com>
2021-01-24 21:23:13 +0900

    210124

--- 2021/210124.py
+++ 2021/210124.py
@@ -1,3 +1,5 @@
+# https://bluese05.tistory.com/78
+
 # function annotation
 
 # function annotation은 python3 이상에서 사용 가능하다.
@@ -19,9 +21,50 @@
 # annotation의 가장 큰 특징은 바로 강제성이 없다는 것이다.
 # 즉, annotation이라는 말그대로 주석일 뿐이며 해당 code 자체에는 어떠한 영향도 미치지 않는다.
 
-def func(arg1: str, arg2: 1+2, arg3: '')
+def func(arg1: str, arg2: 1+2, arg3: 'this is annotation') -> bool:
+    print(f'arg1 = {arg1}')
+    print(f'arg2 = {arg2}')
+    print(f'arg3 = {arg3}')
+    return True
+
+result = func('test1', 3, 'this is test')
+print(f'result = {result}')
+
+# annotation에 적혀 있는 내용은 주석이므로 실제로는 이와 상관없이 실행되게 된다.
+
+def func1(arg1: str, arg2: 1+2, arg3: 'this is annotation') -> bool:
+    print(f'arg1 = {arg1}')
+    print(f'arg2 = {arg2}')
+    print(f'arg3 = {arg3}')
+    return 'melong'
+
+result = func1('test1', 'test2', 'test3')
+print(f'result = {result}')
+
+# function annotation의 장점
+# annotation의 장점이라고 한다면, 함수의 매개변수와 반환값을 명시적으로 보여준다는데 있다.
+# python은 언어의 특성상 변수의 자료형을 명시적으로 지정하지 않기 때문에 사용성이 간편하고 유연한 대신 코드의 명확성은 떨어지는 단점이 있다.
+# 보통 그래서 function을 작성할때 아래와 같이 주석(comment)를 달아 개발자간 커뮤니케이션에 쓰이긴 하지만 이것 또한 지속적으로 작성하기 쉽지 않기 때문이다.
 
 
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+# Function with comments
+
+def func(arg1, arg2, arg3):
+    ```
+    :param arg1: str
+    :param arg2: str
+    :param arg3: list
+    :retgurn: bool
+    ```
+
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+sol1 = Solution()
+
+result = sol1.twoSum([2,7,11,15], 9)
+print(f'result = {result}')
+
+    
